5e0ffdf02cca1bd754570f1e706da242d0c8d5eb
nurlan <baitassovnurlan1507@gmail.com>
2024-11-11 23:35:08 +0600

    feat:Open API docs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const openApiDocument = yaml.load(fs.readFileSync('./doc/api.yaml', 'utf8'));
+  SwaggerModule.setup('api', app, openApiDocument);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
+      whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true, 
+      transform: true,
     }),
   );
 
